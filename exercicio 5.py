c3af5d7872ee04ff7527cf1ce5dc6fac3489b619
spark1235 <12andrefcc@gmail.com>
2025-09-30 11:58:52 +0100

    exercisio:5 update

--- exercicio 5.py
+++ exercicio 5.py
@@ -2,12 +2,12 @@ nome=input("digite seu nome:")
 sexo=input("digite seu sexo:(M/F):")
 altura=float(input("digite sua altura(exemplo 1.64):"))
 
-if sexo == "M":
+if sexo == "M" or sexo == "m":
     if altura >= 1.74:
         print("O", nome, "é alto.")
     else:
         print("O", nome, "é baixo.")
-elif sexo == "F":
+elif sexo == "F" or sexo == "f":
     if altura >= 1.64:
         print("A", nome, "é alta.")
     else:
