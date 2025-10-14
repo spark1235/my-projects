24fa83b0ff5a4cfdd538165a82200b9ad7874e4e
spark1235 <12andrefcc@gmail.com>
2025-10-14 10:18:52 +0100

    exercicio 11:update

--- exercicio 11.py
+++ exercicio 11.py
@@ -14,30 +14,53 @@ class Cao(Animal):
     def latir(self):
         print(f"{self.nome} está a latir: Au Au!")
 
+class Animal:
+    def __init__(self, nome, cor, peso):
+        self.nome = nome
+        self.cor = cor
+        self.peso = peso
+
+    def andar(self):
+        print(f"{self.nome} está a andar.")
+
+    def correr(self):
+        print(f"{self.nome} está a correr.")
+
+class Cao(Animal):
+    def latir(self):
+        print(f"{self.nome} está a latir: Au au!")
+
 class Gato(Animal):
     def miar(self):
         print(f"{self.nome} está a miar: Miau!")
 
-print("Informações do Cão")
+# Entrada dos dados pelo utilizador
+print("Criação de um cão:")
 nome_cao = input("Nome do cão: ")
 cor_cao = input("Cor do cão: ")
-peso_cao = input("Peso do cão (kg): ")
+peso_cao = float(input("Peso do cão (kg): "))
+cao1 = Cao(nome_cao, cor_cao, peso_cao)
 
-cao = Cao(nome_cao, cor_cao, peso_cao)
-
-print("\nInformações do Gato")
+print("\nCriação de um gato:")
 nome_gato = input("Nome do gato: ")
 cor_gato = input("Cor do gato: ")
-peso_gato = input("Peso do gato (kg): ")
+peso_gato = float(input("Peso do gato (kg): "))
+gato1 = Gato(nome_gato, cor_gato, peso_gato)
 
-gato = Gato(nome_gato, cor_gato, peso_gato)
+print("\nCriação de outro cão:")
+nome_cao2 = input("Nome do outro cão: ")
+cor_cao2 = input("Cor do outro cão: ")
+peso_cao2 = float(input("Peso do outro cão (kg): "))
+cao2 = Cao(nome_cao2, cor_cao2, peso_cao2)
 
-print("\nAções do cão:")
-cao.andar()
-cao.correr()
-cao.latir()
+print("\nAções dos animais:")
 
-print("\nAções do gato:")
-gato.andar()
-gato.correr()
-gato.miar()
+# 2 ações para cada animal
+cao1.andar()
+cao1.latir()
+
+gato1.correr()
+gato1.miar()
+
+cao2.andar()
+cao2.correr()
